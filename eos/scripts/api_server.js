7a5032adfb74612b4e3278a682d8d79fcb9343f5
mtm <mtadeunet@gmail.com>
2018-05-31 16:01:05 -0700

    Update to the emanate REST API

--- eos/scripts/api_server.js
+++ eos/scripts/api_server.js
@@ -2,12 +2,14 @@
 
 var fs = require("fs");
 var path = require('path');
-var Eos = require('eosjs')
-const express = require('express')
-const app = express()
+var Eos = require('eosjs');
+const express = require('express');
+var cors = require('cors');
+const app = express();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 function getKeyPair(accountName, level) {
 
@@ -69,8 +71,10 @@ function resultError(data) {
 function getEOSConfig() {
     return {
         httpEndpoint: 'http://localhost:8888',
+        binaryen: require("binaryen"),
         keyProvider: [
             getKeyPair('emancollab').private,
+            getKeyPair('emancontent').private,
             getKeyPair('collabuser1').private,
             getKeyPair('collabuser2').private,
             getKeyPair('collabuser3').private,
@@ -99,20 +103,6 @@ app.get('/api', function(req, res) {
 });
 
 
-/*
- * {
- *     "from": <colaboration contract creator>
- *     "name": <colaboration contract name>
- *     "price": <execution price>
- *     "partners": [{
- *         "name": <partner name>
- *         "percentage": <royalty percentage>
- *     }]
- * }
- * 
- * http://localhost:85/propose/
- * {"from": "collabuser1", "name": "contract1", "price": 10000, "partners": [{"name": "collabuser2", "percentage": 10}]}
- * */
 app.post('/propose', (req, res) => {
     var data = req.body;
     const options = eosCallOptions(["emancollab"], [permissions("emancollab"), permissions(data.name)]);
@@ -128,16 +118,6 @@ app.post('/propose', (req, res) => {
     });
 })
 
-/*
- * {
- *     "from": <accout accepting the contract>
- *     "proposer": <accout that proposed the contract>
- *     "contract": <contract name>
- * }
- * 
- * http://localhost:85/accept/
- * {"from": "collabuser2", "proposer": "collabuser1", "name": "contract1"}
- * */
 app.post('/accept', bodyParser.json(), (req, res) => {
     var data = req.body;
     const options = eosCallOptions(["emancollab"], [permissions("emancollab")]);
@@ -154,16 +134,6 @@ app.post('/accept', bodyParser.json(), (req, res) => {
     
 })
 
-/*
- * {
- *     "from": <accout rejecting the contract>
- *     "proposer": <accout that proposed the contract>
- *     "contract": <contract name>
- * }
- * 
- * http://localhost:85/reject/
- * {"from": "collabuser2", "proposer": "collabuser1", "name": "contract1"}
- * */
 app.post('/reject', (req, res) => {
     var data = req.body;
     const options = eosCallOptions(["emancollab"], [permissions("emancollab")]);
@@ -179,29 +149,14 @@ app.post('/reject', (req, res) => {
     });
 })
 
-/*
- * {
- *     "from": <accout canceling the contract>
- *     "proposer": <accout that proposed the contract>
- *     "contract": <contract name>
- * }
- * */
 app.post('/cancel', (req, res) => {
     var data = req.body;
     
     res.send("Canceling the proposal");
 })
 
-/*
- * {
- *     "from": <accout executing the contract>
- *     "proposer": <accout that proposed the contract>
- *     "contract": <contract name>
- * }
- * */
 app.post('/execute', (req, res) => {
     var data = req.body;
-//     const options = eosCallOptions(["emancollab"], [{account: "emancollab", permission: "active"}]);
     const options = eosCallOptions(["emancollab"], [permissions("emancollab")]);
     
     eos.contract('emancollab', options).then(contract => {
@@ -215,24 +170,46 @@ app.post('/execute', (req, res) => {
     });
 })
 
-
-// app.post('/getBalance', (req, res) => {
-//     var data = JSON.parse(req.query.data);
-// })
-
-
-/*
- * {
- *     "proposer": <accout that proposed the contract>
- *     "contract": <contract name>
- * }
- * */
 app.post('/getContract', (req, res) => {
     var data = req.body;
 
     console.log(data);
     eos.getTableRows(true, 'emancollab', data.proposer, 'proposal').then(results => {
         //  TODO: Go through the results and find the proper contract from this user
+        res.send(resultOk(results));
+    }).catch(error => {
+        res.send(resultError(error));
+    });
+})
+
+app.post('/addTrack', (req, res) => {
+    var data = req.body;
+    const options = eosCallOptions(["emancontent"], [permissions("emancontent")]);
+
+    console.log("step 1");
+    eos.contract('emancontent', options).then(contract => {
+        console.log("step 2");
+        contract.addtrack(data.owner, data.metadata).then(function() { 
+            console.log("step 3");
+            res.send(resultOk());
+            console.log("step 4");
+        }).catch(error => {
+            console.log("step 5");
+            res.send(resultError(error));
+            console.log("step 6");
+        });
+    }).catch(error => {
+        console.log("step 7 - " + error);
+        res.send(resultError(error));
+        console.log("step 8");
+    });
+    console.log("step 9");
+})
+
+app.post('/getTracks', (req, res) => {
+    var data = req.body;
+
+    eos.getTableRows(true, 'emancontent', data.owner, 'track').then(results => {
         res.send(resultOk(results));
     }).catch(error => {
         res.send(resultError(error));
